35e778df4d7908cfdd8b1dcae5143b363459a2ff
scientific-dev <69678617+Scientific-Guy@users.noreply.github.com>
2022-05-23 13:43:24 +0530

    initial commit

---
+++ src/components/Board.svelte
@@ -0,0 +1,217 @@
+<script>
+    import { onMount } from 'svelte';
+    import touchToMouse from 'svelte-touch-to-mouse';
+    import Board from '../utils/Board';
+
+    let board = { running: false, rows: 10, columns: 10, scrollOffset: 40 }, 
+        generations = 0,
+        speed = 1.5,
+        cellSize = 50,
+        grids = '0x0',
+        displayInfo = false;
+
+    function plotExample (fn) {
+        fn();
+        board = board;
+        generations = 0;
+        displayInfo = false;
+    }
+
+    onMount(() => {
+        let sOf = () => board.scrollOffset = window.innerWidth > 1024 ? 40 : 150;
+        board = new Board(document.getElementById('canvas'));
+        board.onEachGen = () => generations = board.generations;
+
+        window.board = board;
+        window.addEventListener('resize', () => {
+            board.resize();
+            sOf();
+        });
+
+        speed = board.speed / 1000;
+        cellSize = board.cellSize;
+        grids = `${board.rows}x${board.columns}`;
+
+        touchToMouse('canvas');
+        sOf();
+    });
+
+    $: board.speed = speed * 1000;
+    $: {
+        board.cellSize = cellSize;
+        board.resize?.();
+    }
+
+    $: {
+        let f = (x, y) => isNaN(x = parseInt(x)) ? y : x;
+        let [r, c] = grids.split('x');
+
+        board.rows = f(r, board.rows);
+        board.columns = f(c, board.columns);
+        board.resize?.();
+    }
+</script>
+
+<div style={displayInfo ? 'opacity: 0.7;' : ''}>  
+    <canvas 
+        id="canvas"
+        width={window.innerWidth}
+        height={window.innerHeight}
+    />
+
+    <h1 class="float">Game of Life</h1>
+
+    <div class="float panel">
+        <a 
+            href="#start"
+            on:click={() => {
+                board[board.running ? 'stop' : 'start']();
+                board = board;
+            }}
+        >{board.running ? 'Stop' : 'Start'}</a>
+
+        {#if !board.running}
+            <a href="#start" on:click={() => board.nextGeneration()}>Next</a>
+            <a href="#start" on:click={() => board.fillBg().drawGame()}>Clear</a>
+        {/if}
+
+        <a href="#start">Speed: <input type="number" min=0.1 bind:value={speed}/>s</a>
+        <a href="#start">Size: <input type="number" min=5 bind:value={cellSize}/></a>
+        <a href="#start">Grids: <input bind:value={grids}/></a>
+
+        <span>Generations: {generations}</span>
+        <a href="#info" on:click={() => displayInfo = true}>i</a>
+    </div>
+
+</div>
+
+{#if displayInfo}
+    <div class="infotab float">
+        <h2>Game of Life</h2>
+
+        <p>
+            The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.
+            More in <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">Wikipedia</a>.
+        </p>
+
+        <h3>Rules</h3>
+
+        <ol>
+            <li>Each cell has 8 neighbours surrounding it.</li>
+            <li>Coloured cells represents that the cell is alive while uncoloured cells represents that the cell is dead.</li>
+            <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+            <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+            <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+            <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+        </ol>
+
+        <h3>Examples:</h3>
+
+        <div style="margin-left: 10px; margin-top: -10px;">
+            <a 
+                class="info-btn" 
+                href="#start" 
+                on:click={() => plotExample(() => {
+                    board.plotExample('GOSPER_GLIDER_GUN')
+                    grids = '50x50';
+                })}
+            >Gosper Glider Gun</a>
+
+            <a class="info-btn" href="#start" on:click={() => plotExample(() => board.plotRandom())}>Random</a>
+        </div>
+
+        <br/>
+        <a class="info-btn" href="#default" on:click={() => displayInfo = false}>Close</a>
+
+        <hr/>
+
+        <p>
+            Made by <a target="_blank" href="https://github.com/scientific-dev">TheSudarsanDev</a><br/>
+            TheSudarsanDev © {new Date().getFullYear()}
+        </p>
+    </div>
+{/if}
+
+<style>
+    canvas {
+        width: 100vw;
+        height: 100vh;
+        -webkit-touch-callout: none;
+        -ms-touch-action: none; 
+        touch-action: none;
+    }
+
+    .float {
+        z-index: 1;
+        position: fixed;
+    }
+
+    h1 {
+        margin: 0;
+        font-size: 25px;
+        left: 10px;
+        top: 10px;
+        user-select: none;
+    }
+
+    .panel {
+        bottom: 11px;
+        left: 16px;
+        margin-right: 8px;
+    }
+
+    .panel a, .panel span, .info-btn {
+        text-decoration: none;
+        padding: 5px 10px;
+        border-radius: 3px;
+        margin-left: 2px;
+        margin-bottom: 4px;
+        display: inline-block;
+    }
+
+    .panel a, .panel span {
+        background-color: white;
+        color: black;
+    }
+
+    .panel a:hover, .panel span:hover {
+        background-color: whitesmoke;
+    }
+
+    .info-btn {
+        background-color: #18181d;
+        color: white!important;;
+    }
+
+    input {
+        outline: none;
+        color: white;
+        background-color: #18181d;
+        border: none;
+        border-radius: 2px;
+        width: 50px;
+        margin-top: -5px;
+        display: inline-block;
+    }
+
+    .infotab {
+        background-color: whitesmoke;
+        padding: 25px;
+        border-radius: 4px;
+        top: 20px;
+        left: 20px;
+        right: 20px;
+        bottom: 20px;
+        color: black!important;
+        overflow-y: auto;
+    }
+
+    .infotab h2 {
+        margin: 0;
+    }
+
+    .infotab a {
+        color: black;
+        font-weight: bold;
+    }
+</style>
---
+++ src/utils/Board.js
@@ -0,0 +1,222 @@
+export default class Board {
+
+    data = {}
+    cellSize = 50;
+    initialRow = 0;
+    initialColumn = 0;
+    generations = 0;
+    rows = 0;
+    columns = 0;
+    speed = 1500;
+    offsetX = 0;
+    offsetY = 0;
+    scrollOffset = 40;
+    running = false;
+    onEachGen = () => {};
+
+    constructor (canvas = new HTMLCanvasElement()) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.resize();
+
+        canvas.onmousedown = ({ pageX, pageY }) => {
+            let cX = pageX, cY = pageY;
+
+            const onmove = (event) => {
+                let o = this.offset, so = this.scrollOffset;
+                
+                this.offsetX += cX - event.pageX;
+                this.offsetY += cY - event.pageY;
+
+                this.offsetX = Math.max(-so, Math.min(this.offsetX, (this.columns * o - canvas.width) + so));
+                this.offsetY = Math.max(-so, Math.min(this.offsetY, (this.rows * o - canvas.height) + so));
+
+                cX = event.pageX, cY = event.pageY;
+                this.fillBg().drawGame().plotFromData(this.data);
+                this.canvas.style.cursor = 'grabbing';
+            }
+
+            const onup = () => {
+                canvas.removeEventListener('mousemove', onmove);
+                canvas.removeEventListener('mouseup', onup);
+                this.canvas.style.cursor = 'default';
+            }
+
+            canvas.addEventListener('mousemove', onmove);
+            canvas.addEventListener('mouseup', onup);
+        };
+
+        canvas.onclick = (event) => {
+            if (this.running) return;
+            let o = this.cellSize + this.ctx.lineWidth,
+                c = Math.floor((event.pageX + this.offsetX) / o),
+                r = Math.floor((event.pageY + this.offsetY) / o)
+            
+            if (c < 0 || r < 0) return;
+            if (this.columns > c && this.rows > r) this.plot(c, r);
+        };
+    }
+
+    get offset () {
+        return this.cellSize + this.ctx.lineWidth;
+    }
+
+    fillBg () {
+        this.ctx.fillStyle = '#18181d';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fill();
+
+        return this;
+    }
+
+    drawGame () {
+        let o = this.offset,
+            w = this.columns * o,
+            h = this.rows * o;
+
+        for (let x = 0; x <= this.columns; x++) this.drawLine(x * o, 0, x * o, h);
+        for (let y = 0; y <= this.rows; y++) this.drawLine(0, y * o, w, y * o);
+
+        return this;
+    }
+
+    drawLine (x, y, lx, ly) {
+        this.ctx.strokeStyle = '#ffffff';
+        this.ctx.beginPath();
+        this.ctx.moveTo(x - this.offsetX, y - this.offsetY);
+        this.ctx.lineTo(lx - this.offsetX, ly - this.offsetY);
+        this.ctx.stroke();
+    }
+
+    resize () {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx.lineWidth = (this.cellSize > 20) ? 1 : 0.5;
+
+        if (!this.rows && !this.columns) {
+            this.rows = Math.ceil(window.innerHeight / this.offset);
+            this.columns = Math.ceil(window.innerWidth / this.offset);
+        }
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.fillBg()
+            .drawGame()
+            .plotFromData(this.data);
+    }
+
+    plot (column, row) {
+        let r = this.data[row] || (this.data[row] = new Set()),
+            alive = !r.has(column);
+
+        r[alive ? 'add' : 'delete'](column);
+        this.fillCell(column, row, alive ? '#ffffff' : '#18181d')
+    }
+
+    plotFromData (data) {
+        let rows = Object.entries(data);
+
+        for (let i = 0; i < rows.length; i++) {
+            let [r, columns] = rows[i];
+            for (let c of columns)
+                this.fillCell(c, r, '#ffffff');
+        }
+    }
+
+    fillCell (column, row, color) {
+        let o = this.offset,
+            lw = this.ctx.lineWidth;
+
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect((column * o) + lw - this.offsetX, (row * o) + lw - this.offsetY, this.cellSize - lw, this.cellSize - lw);
+        this.ctx.fill();     
+    }
+
+    nextGeneration () {
+        let newData = {},
+            lastRow = new Set(),
+            currentRow = new Set(),
+            nextRow = this.data[0] || new Set();
+
+        for (let r = 0; r < this.rows; r++) {
+            let columns = new Set(nextRow),
+                lastCol = 0,
+                currentCol = 0,
+                nextCol = columns.has(0);
+
+            currentRow = nextRow;
+            nextRow = this.data[r + 1] || new Set();
+
+            for (let c = 0; c < this.columns; c++) {
+                currentCol = nextCol;
+                nextCol = columns.has(c + 1);
+
+                let n = lastCol + nextCol;
+                for (let i = -1; i < 2; i++) 
+                    n += lastRow.has(c + i) + nextRow.has(c + i);
+
+                if (n == 3) {
+                    columns.add(c);
+                    this.fillCell(c, r, '#ffffff');
+                } else if (n != 2) {
+                    columns.delete(c);
+                    this.fillCell(c, r, '#18181d');
+                }
+
+                lastCol = currentCol;
+            }
+
+            newData[r] = columns;
+            lastRow = currentRow;
+        }
+
+        this.data = newData;
+        this.generations += 1;
+        this.onEachGen();
+    }
+
+    async start () {
+        this.running = true;
+
+        while (this.running) {
+            this.nextGeneration();
+            await new Promise(r => setTimeout(r, this.speed));
+        }
+    }
+
+    stop () {
+        this.running = false;
+    }
+
+    insertRawData (data) {
+        this.data = Object.fromEntries(Object.entries(data).map(([k, v]) => [k, new Set(v)]))
+        this.plotFromData(this.data);
+    }
+
+    getRawData () {
+        return JSON.stringify(Object.fromEntries(Object.entries(this.data).map(([k, v]) => [k, Array(...v)]))); 
+    }
+
+    plotExample (id) {
+        this.insertRawData(EXAMPLES[id]);
+    }
+
+    plotRandom () {
+        let f = () => Math.floor(Math.random() * 2);
+
+        for (let r = 0; r < this.rows; r++) {
+            let columns = new Set();
+
+            for (let c = 0; c < this.columns; c++)
+                if (f()) columns.add(c);
+
+            this.data[r] = columns;
+        }
+
+        this.plotFromData(this.data);
+    }
+
+}
+
+const EXAMPLES = {
+    GOSPER_GLIDER_GUN: {"1":[24],"2":[22,24],"3":[12,13,20,21,34,35],"4":[11,15,20,21,34,35],"5":[0,1,10,16,21,20],"6":[0,1,10,14,16,17,22,24],"7":[10,16,24],"8":[11,15],"9":[12,13]}
+};
